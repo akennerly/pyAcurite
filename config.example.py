d6b00386e4adc263ad703fff491abd4a011080fb
Andrew Kennerly <andrew@kennerly.net>
2020-11-02 00:17:14 -0500

    REMOVED: config.example.py - No longer needed

--- config.example.py
+++
@@ -1,6 +0,0 @@
-# CONFIGURATION
-STATION_ID = 'KCASANFR9999'
-STATION_KEY = 'xxxxxxx'
-FREQUENCY = 30 # number of seconds to send updates
-ACURITE_HUB_IP = '52.22.110.201' # the real ip for hubapi.myacurite.com
-SEND_TO_ACURITE = False # send data to acurite, while you still can
